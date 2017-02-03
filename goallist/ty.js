6b2c5a2e94f9fdd527654038e7ce451f2133dca1
Aharon Turpie <aaturpie@verizon.net>
2017-02-03 14:54:31 -0500

    Added Ty goals list

---
+++ goallist/ty.js
@@ -0,0 +1,158 @@
+var bingoList = [];
+bingoList[1] = [
+    { name: "Obtain 3 Boomerangs", types: ["rangs"] },
+    { name: "6 Thunder Eggs in Two Up", types: ["twoup","teggs"] },
+    { name: "All 7 portraits in Two Up", types: ["twoup","portraits"] },
+    { name: "Get 5 cogs in Two Up", types: ["twoup","cogs"] }
+];
+bingoList[2] = [
+    { name: "All Opals in Two Up", types: ["twoup","opals"] },
+    { name: "6 Thunder Eggs in Walk in the Park", types: ["walk","teggs"] },
+    { name: "6 Thunder Eggs in Ship Rex", types: ["ship","teggs"] },
+    { name: "All 6 portraits in Walk in the Park", types: ["walk","portraits"] },
+    { name: "Get 5 cogs in Walk in the Park", types: ["walk","cogs"] },
+    { name: "Get 5 cogs in Ship Rex", types: ["ship","cogs"] }
+];
+bingoList[3] = [
+    { name: "Obtain 10 Golden Cogs", types: ["cogs"] },
+    { name: "Obtain 10 Rainbow Scales", types: ["scales"] },
+    { name: "All 9 portraits in Ship Rex", types: ["ship","portraits"] },
+    { name: "Rescue the Seahorses", types: ["ship"] },
+    { name: "500 Opals Total", types: ["opals"] }
+];
+bingoList[4] = [
+    { name: "Beat Bull", types: ["boss"] },
+    { name: "Obtain 15 bilbies", types: ["bilbies"] },
+    { name: "All opals in Walk in the Park", types: ["walk","opals"] }
+];
+bingoList[5] = [
+    { name: "100% Two Up", types: ["twoup","hundo"] },
+    { name: "Complete 3 Races", types: ["races"] },
+    { name: "All Opals in Ship Rex", types: ["ship","opals"] },
+    { name: "All 3 races in Hub 1", types: ["races"] }
+];
+bingoList[6] = [
+    { name: "100% Walk in the Park", types: ["walk","hundo"] },
+    { name: "100% Ship Rex", types: ["ship","hundo"] },
+    { name: "Obtain 18 Thunder Eggs", types: ["teggs"] }
+];
+bingoList[7] = [
+    { name: "Obtain 25 Golden Cogs", types: ["cogs"] },
+    { name: "Get 25 Portraits", types: ["portraits"] },
+    { name: "Obtain 20 Rainbow Scales", types: ["scales"] }
+];
+bingoList[8] = [
+    { name: "Obtain all Thunder Eggs in hub 1", types: ["teggs"] },
+    { name: "1000 Opals Total", types: ["opals"] },
+    { name: "Listen to Dennis' Voice Mail", types: ["bridge"] },
+    { name: "Beat 3 mini games", types: ["minig"] },
+    { name: "Outback Saffari Village Thunder Egg with Lassarang", types: ["outback"] },
+    { name: "Beat the Outback Escort Mission", types: ["outback"] }
+];
+bingoList[9] = [
+    { name: "All Opals in Bridge on the River Ty", types: ["bridge","opals"] },
+    { name: "Get 5 cogs in Bridge on the River Ty", types: ["bridge","cogs"] },
+    { name: "Beat Neddy the Bully", types: ["minib"] },
+    { name: "Get both turkey Thunder Eggs", types: ["teggs"] },
+    { name: "Beat \"Home Sweet Home\" and \"Heat Dennis' House\"", types: ["bridge"] }
+];
+bingoList[10] = [
+    { name: "6 Thunder Eggs in Outback Safari", types: ["outback","teggs"] },
+    { name: "Obtain 30 bilbies", types: ["bilbies"] },
+    { name: "Get 5 cogs in Snow Worries", types: ["snow","cogs"] },
+    { name: "6 Thunder Eggs in Snow Worries", types: ["snow","teggs"] },
+    { name: "6 Thunder Eggs in Bridge on the River Ty", types: ["bridge","teggs"] },
+    { name: "Get 5 cogs in Outback Safari", types: ["outback","cogs"] }
+];
+bingoList[11] = [
+    { name: "Obtain 40 Golden Cogs", types: ["cogs"] },
+    { name: "Trap the Lobster", types: ["snow"] },
+    { name: "100% Bridge on the River Ty", types: ["bridge","hundo"] },
+    { name: "all opals in Snow Worries", types: ["snow","opals"] }
+];
+bingoList[12] = [
+    { name: "1500 opals total", types: ["opals"] },
+    { name: "Beat Crikey", types: ["boss"] },
+    { name: "Say 'No' to 3 unique propositions", types: [""] },
+    { name: "Obtain 6 boomerangs", types: ["rangs"] }
+];
+bingoList[13] = [
+    { name: "100% Snow Worries", types: ["snow","hundo"] },
+    { name: "All 3 races of area 2", types: ["races"] },
+    { name: "Get 50 Portraits", types: ["portraits"] },
+    { name: "All 20 Portraits in Bridge on the River Ty", types: ["bridge","portraits"] }
+];
+bingoList[14] = [
+    { name: "Obtain 55 Golden Cogs", types: ["cogs"] },
+    { name: "Obtain 36 Thunder Eggs", types: ["teggs"] },
+    { name: "Complete 6 races", types: ["races"] },
+    { name: "all opals in Outback Safari", types: ["outback","opals"] }
+];
+bingoList[15] = [
+    { name: "100% Outback Safari", types: ["outback","hundo"] },
+    { name: "All 24 portraits in Snow Worries", types: ["snow","portraits"] },
+    { name: "Beat Sly", types: ["minib"] }
+];
+bingoList[16] = [
+    { name: "Obatain all Thunder Eggs in area 2", types: ["teggs"] },
+    { name: "6 Thunder Eggs in Beyond the Black Stump", types: ["stump","teggs"] },
+    { name: "All 5 portraits in Lyre, Lyre", types: ["lyre","portraits"] },
+    { name: "Beat all 'story' missions", types: [""] }
+];
+bingoList[17] = [
+    { name: "6 Thunder Eggs in Beyond the Lyre, Lyre", types: ["lyre","teggs"] },
+    { name: "Go to all of Lenny's suggested paths in Lyre, Lyre", types: ["lyre"] },
+    { name: "Beat all 8 mini games", types: ["minig"] },
+    { name: "Get 5 cogs in Lyre, Lyre", types: ["lyre","cogs"] },
+    { name: "Get 75 portraits", types: ["portraits"] },
+    { name: "Complete both Waterslide Races", types: [""] }
+];
+bingoList[18] = [
+    { name: "Obatin Cable Car Capers", types: [""] },
+    { name: "Obtain 70 Golden Cogs", types: ["cogs"] },
+    { name: "All opals in Lyre, Lyre", types: ["lyre","opals"] },
+    { name: "All opals in Beyond the Black Stump", types: ["stump","opals"] },
+    { name: "Complete the Treasure Hunt", types: ["rex"] },
+    { name: "All 3 races of area 3", types: ["races"] }
+];
+bingoList[19] = [
+    { name: "Beat Fluffy", types: ["boss"] },
+    { name: "Obtain >=4 Thunder Eggs in all levels", types: ["teggs"] },
+    { name: "100% Lyre Lyre", types: ["lyre","hundo"] },
+    { name: "2000 opals total", types: ["opals"] },
+    { name: "Get 5 cogs in Rex Marks the Spot", types: ["rex","cogs"] }
+];
+bingoList[20] = [
+    { name: "Obtain >= 3 cogs in all levels", types: ["cogs"] },
+    { name: "Complete both Rex Races", types: ["races"] },
+    { name: "100% Beyond the Black Stump", types: ["stump","hundo"] },
+    { name: "Obtain Double Health", types: ["scales"] }
+];
+bingoList[21] = [
+    { name: "100% Ship Rex", types: ["ship","hundo"] },
+    { name: "Obtain 9 boomerangs", types: ["rangs"] },
+    { name: "Obtain all bilbies", types: ["bilbies"] },
+    { name: "Get 100 Portraits", types: ["portraits"] },
+    { name: "All 18 portraits in Rex Marks the Spot", types: ["rex","portraits"] },
+    { name: "Complete 9 races", types: ["races"] }
+];
+bingoList[22] = [
+    { name: "All 29 portraits in Beyond the Black Stump", types: ["stump","portraits"] },
+    { name: "2500 opals total", types: ["opals"] },
+    { name: "All opals in Rex Marks the Spot", types: ["rex","opals"] }
+];
+bingoList[23] = [
+    { name: "All Opals (2700)", types: ["opals"] },
+    { name: "Obtain 54 Thunder Eggs", types: ["teggs"] }
+];
+bingoList[24] = [
+    { name: "Beat the Game", types: [""] },
+    { name: "Beat Shadow", types: ["minib"] },
+    { name: "Obtain all Golden Cogs", types: ["cogs"] }
+];
+bingoList[25] = [
+    { name: "Obtain all Thunder Eggs in area 3", types: ["teggs"] },
+    { name: "Get 127 Portraits", types: ["portraits"] },
+    { name: "Beat all Mini-bosses", types: ["minib"] }
+];
+$(function() { srl.bingo(bingoList, 5); });
